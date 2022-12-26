6fca28c060ffb0da3e475c0a0afadafa9bb683a3
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-12-26 21:49:50 +0100

    Install dependencies

--- instagram_Influencer/settings.py
+++ instagram_Influencer/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'instagram',
+    'accounts',
+
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,10 @@ WSGI_APPLICATION = 'instagram_Influencer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram_influencer',
+        'USER': 'codepriest',
+        'PASSWORD':'@#$lagos2015',
     }
 }
 
