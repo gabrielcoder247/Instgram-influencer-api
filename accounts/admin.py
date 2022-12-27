c7dfd2ac9d49cb96261ced99c50e76d6f66a3175
gabrielcoder247 <gabrielcoder247@gmail.com>
2022-12-27 09:19:34 +0100

    Add CustomUser amodels

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,47 @@
-from django.contrib import admin
+# from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+# from .forms import UserAdminChangeForm, UserAdminCreationForm
+
+User = get_user_model()
+
+
+class UserAdmin(BaseUserAdmin):
+# The forms to add and change user instances
+		# add_form = UserAdminCreationForm
+		# form = UserAdminChangeForm
+		# inlines = [
+		#     ProfileInline,
+		# ]
+
+		fieldsets = (
+			# (None, {'fields': ('password',)}), # This simply shows the hashed password field
+			(_('Personal info'), {'fields': ('username', 'email',)}),
+			(_('Permissions'), {
+				'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',)
+			}),
+			(_('Important dates'), {'fields': ('last_login', 'date_joined',)}),
+		)
+		# add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+		# overrides get_fieldsets to use this attribute when creating a user.
+		add_fieldsets = (
+			(None, {
+				'classes': ('wide',),
+				'fields': ('username', 'email', 'password1', 'password2')}
+			 ),
+		)
+		list_display = ('id', 'email', 'username', 'is_active', 'is_staff')
+		list_display_links = ['email']
+		list_filter = ('is_superuser', 'is_staff', 'is_active', 'groups')
+		search_fields = ('email', 'username')
+		ordering = ('email',)
+		list_per_page = 10
+		filter_horizontal = ('groups', 'user_permissions',)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
---
+++ instagram/utils.py
@@ -0,0 +1,15 @@
+
+def get_username(self):
+	if isinstance(self, bool):
+		pass
+	# else:
+	# 	names = self.split()
+	# 	return names[0]
+
+
+# def get_last_name(self):
+#     if isinstance(self, bool):
+#         pass
+#     else:
+#         names = self.split()
+#         return names[-1]
